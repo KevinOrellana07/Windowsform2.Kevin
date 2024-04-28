00756a62a4aacf6f68790eebc1c126a974674168
KevinOrellana07 <135291944+KevinOrellana07@users.noreply.github.com>
2024-04-27 22:51:23 -0600

    Add files via upload

---
+++ Forms1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafios
+{
+    public partial class txtInteres : Form
+    {
+        public txtInteres()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double inversionInicial = double.Parse(txtInversionInicial.Text);
+                double tasaInteresAnual = 0.12;
+                int periodoInversionAnos = int.Parse(txtPeriodoInversion.Text);
+
+                double capitalFinal = inversionInicial * Math.Pow(1 + tasaInteresAnual, periodoInversionAnos);
+                double interesesTotales = capitalFinal - inversionInicial;
+
+                lblCapitalFinal.Text = $"Capital Final: {capitalFinal:C2}";
+                lblInteresesTotales.Text = $"Intereses Totales: {interesesTotales:C2}";
+
+                MessageBox.Show($"Intereses Totales: {interesesTotales:C2}", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos válidos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Formsprestamo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prestamo
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double cantidadPrestamo = double.Parse(txtCantidadPrestamo.Text);
+                double tasaInteresMes = double.Parse(txtTasaInteresMes.Text) / 100;
+                int duracionMeses = int.Parse(txtDuracionMeses.Text);
+
+                double cuotaMensual = (cantidadPrestamo * tasaInteresMes) / (1 - Math.Pow(1 + tasaInteresMes, -duracionMeses));
+
+                lblCuotaMensual.Text = $"Cuota Mensual: {cuotaMensual:C2}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos válidos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
